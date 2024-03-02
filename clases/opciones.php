6d1216af63beb89da416032f0c227cddaa0a8a12
Catalina236 <ccatalina304@gmail.com>
2024-03-02 13:10:54 -0500

    Cambios reserva

---
+++ clases/opciones.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Diseño/estilos.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="tituloPrincipal">Bienvenido</h1>
+    <div class="lista">
+        <ul>
+        <li><a href="Usuario/seleccionar.php">Usuarios</a></li>
+        <li><a href="Reserva/seleccionar.php">Reserva</a> </li>
+        <li><a href="Habitacion/seleccionar.php">Habitaciones</a></li>
+        </ul>
+    </div>
+</body>
+</html>
